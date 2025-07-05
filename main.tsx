12f5b334c4a65e54e64369d87653f2a40aedd9a7
NiickRoos <nicole.roos@estudante.ifms.edu.br>
2025-07-05 19:31:58 -0400

    Arrumando as importações (Amanda)

--- main.tsx
+++ main.tsx
@@ -1,35 +1,45 @@
-// main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 
-import AdCadastrar from './Clientes/advogados/ad-cadastrar';
-import AdListar from './Clientes/advogados/ad-listar';
-import ClCadastrar from './Clientes/clientes/cl-cadastrar';
-import ClListar from './Clientes/clientes/cl-listar';
-import PrCadastrar from './Clientes/processos/pr-cadastar';
-import PrListar from './Clientes/processos/pr-listar';
+
+import Processos from './Clientes/processos/processos';
+import Advogados from './Clientes/advogados/advogados';
+import Clientes from './Clientes/clientes/clientes';
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <nav>
-          <Link to="/advogados/cadastrar">Cadastrar Advogado</Link> |{' '}
-          <Link to="/advogados/listar">Listar Advogados</Link> |{' '}
-          <Link to="/clientes/cadastrar">Cadastrar Cliente</Link> |{' '}
-          <Link to="/clientes/listar">Listar Clientes</Link> |{' '}
-          <Link to="/processos/cadastrar">Cadastrar Processo</Link> |{' '}
-          <Link to="/processos/listar">Listar Processos</Link>
+        <nav style={{ marginBottom: '20px' }}>
+          <Link to="/processos">Processos</Link>
+          <Link to="/advogados">advogados</Link>
+          <Link to="/clientes">clientes</Link>
+          
         </nav>
 
         <Routes>
-          <Route path="/advogados/cadastrar" element={<AdCadastrar />} />
+          {/* Rota raiz mostrando mensagem */}
+          <Route path="/" element={<h1>Bem-vindo à aplicação!</h1>} />
+
+          {/* Se preferir redirecionar o / para listar processos, descomente a linha abaixo e comente a anterior */}
+          {/* <Route path="/" element={<Navigate to="/processos/listar" replace />} /> */}
+
+          {/*<Route path="/advogados/cadastrar" element={<AdCadastrar />} />
           <Route path="/advogados/listar" element={<AdListar />} />
           <Route path="/clientes/cadastrar" element={<ClCadastrar />} />
           <Route path="/clientes/listar" element={<ClListar />} />
-          <Route path="/processos/cadastrar" element={<PrCadastrar />} />
-          <Route path="/processos/listar" element={<PrListar />} />
+          */}
+          <Route path="/processos" element={<Processos />} />
+          <Route path="/clientes" element={<Clientes />} />
+          <Route path="/advogados" element={<Advogados />} />
+
+          {//Route path="/processos/listar" element={<PrListar />} />
+          }
+
+          {/* Rota para página não encontrada */}
           <Route path="*" element={<h1>Página não encontrada</h1>} />
         </Routes>
       </div>
